47779b878abda53ee842fee9544c90763772030a
oktayudha05 <oktayudha05@gmail.com>
2023-08-31 07:48:00 +0700

    rapihin folder

--- Algoritma pemrograman struktur data/test.py
+++ Algoritma pemrograman struktur data/test.py
@@ -1,3 +1,11 @@
-hasil = [2,3,4,6]
+class k1:
+    shafira = 85
+    amanda = 80
+    aditya = 75
+    nedia  = 95
+    widya = 85
+    hanif = 75
+    andi = 70
+    dhanar = 85
+    hikma = 80
 
-print(len(hasil))
