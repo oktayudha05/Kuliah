a614bd82a5af466521bff11ddfeccf2e91752b91
oktayudha05 <oktayudha05@gmail.com>
2023-09-01 23:09:09 +0700

    nambahin underscore

--- Algoritma pemrograman struktur data/test.py
+++ Algoritma pemrograman struktur data/test.py
@@ -1,10 +1,13 @@
 tinggi = 5
 
-for n_tinggi in range(tinggi):
-    for j in range(n_tinggi, tinggi):
+for n in range(1,tinggi+1):
+    for j in range(tinggi-n):
         print(' ', end='')
     
-    for j in range(n_tinggi):
-        print('*', end=' ')
+    for j in range(2 * n - 1):
+        if j % 2 == 0:
+            print('*', end='')
+        else:
+            print('_', end='')
 
     print()
