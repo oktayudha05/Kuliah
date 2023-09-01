b1c613f81e6ce64738e03e0abb7d4ec624078254
oktayudha05 <oktayudha05@gmail.com>
2023-09-01 23:10:44 +0700

    menambahkan penjelasan code

--- Algoritma pemrograman struktur data/test.py
+++ Algoritma pemrograman struktur data/test.py
@@ -1,13 +1,16 @@
 tinggi = 5
 
 for n in range(1,tinggi+1):
+    # membuat spasi di awal tiap baris
     for j in range(tinggi-n):
         print(' ', end='')
     
+    # menambahkan bintang dan garis bawah
     for j in range(2 * n - 1):
         if j % 2 == 0:
             print('*', end='')
         else:
             print('_', end='')
 
+    # pindah ke baris selanjutnya
     print()
