883482e7a0b488ea3ee19fcc4e288114d826e517
oktayudha05 <oktayudha05@gmail.com>
2023-09-02 23:51:11 +0700

    fix tugas

--- Algoritma pemrograman struktur data/teori/tugas2.py
+++ Algoritma pemrograman struktur data/teori/tugas2.py
@@ -1,13 +1,13 @@
 tinggi = 5
 
-for n in range(tinggi + 1):
+for n in range(1, tinggi + 1):
     # membuat spasi sebelum bintang
     for s in range(tinggi - n):
-        print(n, end = '')
+        print(' ', end = '')
     
     # menentukan apakah bintang atau garis bawah yang akan di tampilkan
-    for o in range(2 * n - 1):
-        if o % 2 == 0:
+    for s in range(n * 2 - 1):
+        if s % 2 == 0:
             print('*', end = '')
         else:
             print('_', end = '')
