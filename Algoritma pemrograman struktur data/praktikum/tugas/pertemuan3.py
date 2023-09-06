7b1eee2e6e0707e682aae1ef3c52bf07caa39af6
oktayudha05 <oktayudha05@gmail.com>
2023-09-06 10:03:17 +0700

    pertemuan 3

---
+++ Algoritma pemrograman struktur data/praktikum/tugas/pertemuan3.py
@@ -0,0 +1,26 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "\n",
+    "# create me the code to read txt files\n",
+    "# read file\n",
+    "f = open('data.txt', 'r')\n",
+    "print(f.read())\n",
+    "f.close()\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "language_info": {
+   "name": "python"
+  },
+  "orig_nbformat": 4
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
