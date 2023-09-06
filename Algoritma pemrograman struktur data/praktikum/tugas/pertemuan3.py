5529492225a05b7e42a72be6570ea55d5017da54
oktayudha05 <oktayudha05@gmail.com>
2023-09-06 10:10:08 +0700

    py fix

--- Algoritma pemrograman struktur data/praktikum/tugas/pertemuan3.py
+++ Algoritma pemrograman struktur data/praktikum/tugas/pertemuan3.py
@@ -1,26 +1,4 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "\n",
-    "# create me the code to read txt files\n",
-    "# read file\n",
-    "f = open('data.txt', 'r')\n",
-    "print(f.read())\n",
-    "f.close()\n"
-   ]
-  }
- ],
- "metadata": {
-  "language_info": {
-   "name": "python"
-  },
-  "orig_nbformat": 4
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+with open ('pertemuan3.txt', 'r') as readfile:
+    with open ('pertemuan3.txt', 'a') as writefile:
+        for line in readfile:
+            writefile.write(line)
