6241a733d84a241bb4f78cb57fab3162261de0c4
oktayudha05 <oktayudha05@gmail.com>
2023-10-29 20:18:15 +0700

    pull pert 9

--- belajarNabila/looping.py
+++ belajarNabila/looping.py
@@ -1,6 +1,16 @@
-for s in range(2):
-    print('ini s ke ', s)
-    print()
-    for j in range(3):
-        print('ini j ke ', j)
-    print()
+# for s in range(2):
+#     print('ini s ke ', s)
+#     print()
+#     for j in range(3):
+#         print('ini j ke ', j)
+#     print()
+
+def bubub(nama):
+    return 'halo ' + nama + '!'
+
+print(bubub('nabila'))
+
+def bubub2(nama):
+    print('halo ' + nama + '!')
+
+bubub2('nabila')
