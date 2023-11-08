51b018cf6103165099206d4e8f5d4b8ddd7faca2
Oktario Mufti Yudha <oktayudha05@gmail.com>
2023-11-08 03:26:06 +0000

    praktikum 11

---
+++ Algoritma pemrograman struktur data/praktikum/tugas/pertemuan11/importModule.py
@@ -0,0 +1,21 @@
+# Weekly Exercise 1 
+import matematika as mtk
+
+print('weekly exercise 1')
+hitung_luas_lingkaran = mtk.luas_lingkaran(21)
+print(f'   1. ini luas lingkaran mu: {hitung_luas_lingkaran}')
+
+hitung_luas_persegi = mtk.luas_persegi(20)
+print(f'   2. ini luas persegi mu: {hitung_luas_persegi}')
+
+hitung_luas_segitiga = mtk.luas_segitiga(20, 16)
+print(f'   3. ini luas segitiga mu: {hitung_luas_segitiga}')
+
+# Weekly Exercise 2
+import math
+print('weekly exercise 2')
+print(f'   1. phi = {math.pi}')
+print(f'   2. e = {math.e}')
+print(f'   3. cos 0 = {math.cos(0)}')
+
+
---
+++ Algoritma pemrograman struktur data/praktikum/tugas/pertemuan11/matematika.py
@@ -0,0 +1,10 @@
+phi = 3.14
+
+def luas_lingkaran(r):
+    return phi * r * r
+
+def luas_persegi(s):
+    return s * s
+
+def luas_segitiga(a, t):
+    return a * t / 2
