bd51777e24496ea5c944db4c31c168cbe295c68c
oktayudha05 <oktayudha05@gmail.com>
2023-12-07 19:20:59 +0700

    clear table

--- Logika Matematika/tugas-akhir/javascript/nor.js
+++ Logika Matematika/tugas-akhir/javascript/nor.js
@@ -5,6 +5,10 @@ const true2nor = document.querySelector('#true2nor');
 const false2nor = document.querySelector('#false2nor');
 const kirimNor = document.querySelector('#kirimNor');
 const outputNor = document.querySelector('#outputNor');
+const baris1 = document.querySelector('#baris1');
+const baris2 = document.querySelector('#baris2');
+const baris3 = document.querySelector('#baris3');
+const baris4 = document.querySelector('#baris4');
 
 true1nor.addEventListener('click', () => {
     false1nor.classList.remove('active');
@@ -33,6 +37,32 @@ kirimNor.addEventListener('click', () => {
         outputNor.innerHTML = 'Hasil dari <strong>' + input1nor + '</strong> NOR <strong>' + input2nor + '</strong> adalah ' + '<strong>False</strong>'
     } else {
         outputNor.innerHTML = 'Hasil dari <strong>' + input1nor + '</strong> NOR <strong>' + input2nor + '</strong> adalah ' + '<strong>True</strong>'
+    }
+
+    if (input1nor == true && input2nor == true) {
+        baris1.classList.add('table-success');
+
+        baris2.classList.remove('table-success');
+        baris3.classList.remove('table-success');
+        baris4.classList.remove('table-success');
+    } else if (input1nor == false && input2nor == false) {
+        baris4.classList.add('table-success');
+
+        baris1.classList.remove('table-success');
+        baris2.classList.remove('table-success');
+        baris3.classList.remove('table-success');
+    } else if (input1nor == true && input2nor == false) {
+        baris2.classList.add('table-success');
+
+        baris1.classList.remove('table-success');
+        baris3.classList.remove('table-success');
+        baris4.classList.remove('table-success');
+    } else if (input1nor == false && input2nor == true) {
+        baris3.classList.add('table-success');
+
+        baris1.classList.remove('table-success');
+        baris2.classList.remove('table-success');
+        baris4.classList.remove('table-success');
     }
 });
 
--- Logika Matematika/tugas-akhir/javascript/not.js
+++ Logika Matematika/tugas-akhir/javascript/not.js
@@ -3,6 +3,8 @@ const trueNot = document.querySelector('#trueNot');
 const falseNot = document.querySelector('#falseNot');
 const kirimNot = document.querySelector('#kirimNot');
 const outputNot = document.querySelector('#outputNot');
+const baris1 = document.querySelector('#baris1');
+const baris2 = document.querySelector('#baris2');
 
 trueNot.addEventListener('click', () => {
     falseNot.classList.remove('active');
@@ -20,5 +22,15 @@ kirimNot.addEventListener('click', () => {
         outputNot.innerHTML = 'Hasil dari <strong>NOT ' + inputNot + '</strong> adalah ' + '<strong>False</strong>'
     } else {
         outputNot.innerHTML = 'Hasil dari <strong>NOT ' + inputNot + '</strong> adalah ' + '<strong>True</strong>'
+    }
+
+    if (inputNot == true) {
+        baris1.classList.add('table-success');
+
+        baris2.classList.remove('table-success');
+    } else if (inputNot == false) {
+        baris2.classList.add('table-success');
+
+        baris1.classList.remove('table-success');
     }
 });
