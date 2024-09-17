84299a2e8d8e940e2cdeafd1a700b361b3ca01a9
oktayudha05 <oktayudha05@gmail.com>
2024-09-18 00:56:24 +0700

    clean

--- Pemrograman Berorientasi Objek/week4/PraktikumPBO_4/src/tugas4/Tugas4.java
+++ Pemrograman Berorientasi Objek/week4/PraktikumPBO_4/src/tugas4/Tugas4.java
@@ -4,9 +4,12 @@ public class Tugas4 {
   public static void main(String[] args) {
     Pekerja pekerja = new Pekerja("Zidhan", 19, "Tukang Tipu", 1000000);
     
+    System.out.println("====sebelum di ganti====");
+
     System.out.println(pekerja.toString());
     pekerja.setNama("Zidhan Arzaq K.");
     
+    System.out.println("====setelah di ganti====");
     System.out.println(pekerja.toString());
   }
 }
