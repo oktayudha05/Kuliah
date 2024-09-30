e3e58f044bdb85e9214eba800a9364cc44e83d89
oktayudha05 <oktayudha05@gmail.com>
2024-09-30 17:25:25 +0700

    add comment responsi uts pbo

--- Pemrograman Berorientasi Objek/responsiUTS/ResponsiUTS-Oktario_Mufti_Yudha/src/oktario/Main.java
+++ Pemrograman Berorientasi Objek/responsiUTS/ResponsiUTS-Oktario_Mufti_Yudha/src/oktario/Main.java
@@ -2,139 +2,148 @@ package oktario;
 
 // produk
 abstract class Produk {
-  private String namaProduk;
-  private double harga;
+    private String namaProduk;
+    private double harga;
 
-  abstract String tampilkanInfo();
+    abstract String tampilkanInfo();
 
-  public void setNamaProduk(String namaProduk) {
-    this.namaProduk = namaProduk;
-  }
-  public String getNamaProduk() {
-    return this.namaProduk;
-  }
-  public void setHarga(double harga) {
-    this.harga = harga;
-  }
-  public double getHarga() {
-    return this.harga;
-  }
+    public void setNamaProduk(String namaProduk) {
+        this.namaProduk = namaProduk;
+    }
+    public String getNamaProduk() {
+        return this.namaProduk;
+    }
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    public double getHarga() {
+        return this.harga;
+    }
 }
 
 // pegawai
 abstract class Pegawai {
-  private String namaPegawai;
-  private String gaji;
+    private String namaPegawai;
+    private String gaji;
 
-  abstract String tampilkanInfo();
+    abstract String tampilkanInfo();
 
-  public void setNamaPegawai(String namaPegawai) {
-    this.namaPegawai = namaPegawai;
-  }
-  public String getNamaPegawai() {
-    return this.namaPegawai;
-  }
-  public void setGaji(String gaji) {
-    this.gaji = gaji;
-  }
-  public String getGaji() {
-    return this.gaji;
-  }
+    public void setNamaPegawai(String namaPegawai) {
+        this.namaPegawai = namaPegawai;
+    }
+    public String getNamaPegawai() {
+        return this.namaPegawai;
+    }
+    public void setGaji(String gaji) {
+        this.gaji = gaji;
+    }
+    public String getGaji() {
+        return this.gaji;
+    }
 }
 
 // turunan produk
 class Elektronik extends Produk {
-  private int garansi;
+    private int garansi;
 
-  public void setGaransi(int garansi) {
-    this.garansi = garansi;
-  }
-  public int getGaransi() {
-    return this.garansi;
-  }
+    public void setGaransi(int garansi) {
+        this.garansi = garansi;
+    }
+    public int getGaransi() {
+        return this.garansi;
+    }
 
-  @Override
-  public String tampilkanInfo() {
-    return "Nama Produk: " + getNamaProduk() + "\nHarga: " + getHarga() + "\nGaransi: " + garansi;
-  }
+    @Override
+    public String tampilkanInfo() {
+        return "Nama Produk: " + getNamaProduk() + "\nHarga: " + getHarga() + "\nGaransi: " + garansi + " tahun";
+    }
 }
 
 class Makanan extends Produk {
-  private String tanggalKadaluarsa;
+    private String tanggalKadaluarsa;
 
-  public void setTanggalKadaluarsa(String tanggalKadaluarsa) {
-    this.tanggalKadaluarsa = tanggalKadaluarsa;
-  }
-  public String getTanggalKadaluarsa() {
-    return this.tanggalKadaluarsa;
-  }
+    public void setTanggalKadaluarsa(String tanggalKadaluarsa) {
+        this.tanggalKadaluarsa = tanggalKadaluarsa;
+    }
+    public String getTanggalKadaluarsa() {
+        return this.tanggalKadaluarsa;
+    }
 
-  @Override
-  public String tampilkanInfo() {
-    return "Nama Produk: " + getNamaProduk() + "\nHarga: " + getHarga() + "\nTanggal Kadaluarsa: " + tanggalKadaluarsa;
-  }
+    @Override
+    public String tampilkanInfo() {
+        return "Nama Produk: " + getNamaProduk() + "\nHarga: " + getHarga() + "\nTanggal Kadaluarsa: " + tanggalKadaluarsa;
+    }
 }
 
 // turunan pegawai
 class PegawaiTetap extends Pegawai {
-  private String tunjangan;
+    private String tunjangan;
 
-  public void setTunjangan(String tunjangan) {
-    this.tunjangan = tunjangan;
-  }
-  public String getTunjangan() {
-    return this.tunjangan;
-  }
+    public void setTunjangan(String tunjangan) {
+        this.tunjangan = tunjangan;
+    }
+    public String getTunjangan() {
+        return this.tunjangan;
+    }
 
-  @Override
-  public String tampilkanInfo() {
-    return "Nama Pegawai: " + getNamaPegawai() + "\nGaji: " + getGaji() + "\nTunjangan: " + tunjangan;
-  }
+    @Override
+    public String tampilkanInfo() {
+        return "Nama Pegawai: " + getNamaPegawai() + "\nGaji: " + getGaji() + "\nTunjangan: " + tunjangan;
+    }
 }
 
 class PegawaiKontrak extends Pegawai {
-  private String lamaKontrak;
+    private String lamaKontrak;
 
-  public void setLamaKontrak(String lamaKontrak) {
-    this.lamaKontrak = lamaKontrak;
-  }
-  public String getLamaKontrak() {
-    return this.lamaKontrak;
-  }
+    public void setLamaKontrak(String lamaKontrak) {
+        this.lamaKontrak = lamaKontrak;
+    }
+    public String getLamaKontrak() {
+        return this.lamaKontrak;
+    }
 
-  @Override
-  public String tampilkanInfo() {
-    return "Nama Pegawai: " + getNamaPegawai() + "\nGaji: " + getGaji() + "\nLama Kontrak: " + lamaKontrak;
-  }
+    @Override
+    public String tampilkanInfo() {
+        return "Nama Pegawai: " + getNamaPegawai() + "\nGaji: " + getGaji() + "\nLama Kontrak: " + lamaKontrak;
+    }
 }
 
 public class Main {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        
+        // 1. Output Produk Elektronik
+        Produk elektronik = new Elektronik();
+        elektronik.setNamaProduk("Laptop");
+        elektronik.setHarga(5000000);
+        ((Elektronik) elektronik).setGaransi(2);
+        System.out.println("1. Output Produk");
+        System.out.println(elektronik.tampilkanInfo());
+        System.out.println();
 
-    // objek produk
-    Produk elektronik = new Elektronik();
-    elektronik.setNamaProduk("Laptop");
-    elektronik.setHarga(5000000);
-    ((Elektronik) elektronik).setGaransi(1);
-    System.out.println(elektronik.tampilkanInfo());
+        // 2. Output Pegawai Tetap
+        Pegawai pegawaiTetap = new PegawaiTetap();
+        pegawaiTetap.setNamaPegawai("Budi");  // Ganti dengan nama kalian
+        pegawaiTetap.setGaji("5000000");
+        ((PegawaiTetap) pegawaiTetap).setTunjangan("1000000");
+        System.out.println("2. Output Pegawai");
+        System.out.println(pegawaiTetap.tampilkanInfo());
+        System.out.println();
 
-    Produk makanan = new Makanan();
-    makanan.setNamaProduk("Mie Instan");
-    makanan.setHarga(3000);
-    ((Makanan) makanan).setTanggalKadaluarsa("2022-12-31");
-    System.out.println(makanan.tampilkanInfo());
+        // 3. Output Polimorfisme Produk dan Pegawai
+        System.out.println("3. Output Polimorfisme");
 
-    // objek pegawai
-    Pegawai pegawaiTetap = new PegawaiTetap();
-    pegawaiTetap.setNamaPegawai("Budi");
-    pegawaiTetap.setGaji("5000000");
-    ((PegawaiTetap) pegawaiTetap).setTunjangan("1000000");
-    System.out.println(pegawaiTetap.tampilkanInfo());
+        // Polimorfisme Produk Makanan
+        Produk snack = new Makanan();
+        snack.setNamaProduk("Snack");
+        snack.setHarga(15000);
+        ((Makanan) snack).setTanggalKadaluarsa("2023-12-30");
+        System.out.println(snack.tampilkanInfo());
 
-    Pegawai pegawaiKontrak = new PegawaiKontrak();
-    pegawaiKontrak.setNamaPegawai("Andi");
-    pegawaiKontrak.setGaji("3000000");
-    ((PegawaiKontrak) pegawaiKontrak).setLamaKontrak("3 bulan");
-    System.out.println(pegawaiKontrak.tampilkanInfo());
-  }
+        // Polimorfisme Pegawai Kontrak
+        Pegawai pegawaiKontrak = new PegawaiKontrak();
+        pegawaiKontrak.setNamaPegawai("Andi");
+        pegawaiKontrak.setGaji("3000000");
+        ((PegawaiKontrak) pegawaiKontrak).setLamaKontrak("12 bulan");
+        System.out.println(pegawaiKontrak.tampilkanInfo());
+    }
 }
